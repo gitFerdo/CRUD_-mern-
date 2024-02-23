46d0c14402b17d0d68dd439d3c23a9d1a2aa56ea
gitFerdo <fernandothushini8@gmail.com>
2024-02-23 23:35:50 +0530

    Initial commit 2

--- server/Server.js
+++ server/Server.js
@@ -1,13 +1,21 @@
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
 const cors = require( 'cors' );
+const UserModel = require( './models/Users' );
 
 const app = express();
 
 app.use( cors() );
 app.use( express.json() );
 
-//mongoose.connect( "" )
+mongoose.connect( "mongodb+srv://crud:crud@atlascluster.mr5bpcu.mongodb.net/crud" )
+
+app.post( "/createUser", ( req, rse ) =>
+{
+    UserModel.create( req.body )
+        .then( users => res.json( users ) )
+        .catch( err => res.json( err ) )
+} )
 
 app.listen( 3001, () =>
 {
